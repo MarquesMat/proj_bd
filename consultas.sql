406e9d87460dceeb5d5236abc5b6464aefaba139
MarquesMat <90152478+MarquesMat@users.noreply.github.com>
2023-12-13 16:06:19 -0300

    Create consultas.sql

---
+++ consultas.sql
@@ -0,0 +1,44 @@
+--- 5. Selecionar informações de pessoas nível Usuário responsáveis por mais equipamentos
+select distinct u.id, nome, num as telefone, count(mac_equipamento) as total_equipamentos
+from usuario u join telefone_contato t on u.id = t.id_usuario
+left join responsavel_equipamento r on u.id=r.id_usuario
+where id_nivel = (select id from nivel where tipo like 'Usuário')
+group by u.id, nome, num;
+
+--- 6. Calcular razão entre número de chamados e número de equipamentos por localização
+select l.nome, (count(ce.id_chamado)/count(ce.mac_equipamento)) as razao from localizacao l, chamado c, chamado_equipamento ce
+where l.id = c.id_localizacao and c.id = ce.id_chamado
+group by l.nome
+order by razao;
+
+--- 7. Selecionar equipamentos com chamados de nível "urgência alta" ordenado por quantidade.
+select count(c.id) as urgentes, ip from equipamento e, chamado c, chamado_equipamento ce
+where e.mac = ce.mac_equipamento and c.id = ce.id_chamado and c.urgencia like 'Alta'
+group by ip
+order by urgentes;
+
+--- 8. Proporção de chamados em aberto no ano de 2023
+select 
+  extract(year from data) as ano,
+  sum(case when estado = 'Aberto' then 1 else 0 end) as chamados_abertos,
+  count(*) as total_chamados,
+  case when count(*) > 0 then 
+    (sum(case when estado = 'Aberto' then 1 else 0 end)::decimal / count(*)) 
+  else 
+    0 
+  end as fracao_chamados_abertos
+from public.chamado
+where extract(year from data) = 2023
+group by extract(year from data);
+
+--- 9. Selecionar cpf, nome, telefone e nivel de usuário perto de se aposentar (nasceu antes 1965)
+select cpf, nome, tipo as cargo from usuario u, nivel n
+where u.id_nivel = n.id and extract(year from dt_nasc) < 1965
+order by dt_nasc;
+
+--- 10. Selecionar marcas com menor número de chamados
+select marca, count(id_chamado) as total_chamados from equipamento left join chamado_equipamento
+on mac = mac_equipamento -- O left join permite incluir os equipamentos sem chamados
+group by marca
+order by total_chamados;
+
